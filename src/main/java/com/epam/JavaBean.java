61008479780b4e31619137833732b3f3b2624af1
Andrei_Rohau <rohau.andrei@gmail.com>
2021-08-04 16:22:16 +0300

    lesson_03

--- src/main/java/com/epam/JavaBean.java
+++ src/main/java/com/epam/JavaBean.java
@@ -6,8 +6,8 @@ public class JavaBean {
     private Boolean isWoman = false;
     private final TmpClass tmpClass = new TmpClass();
 
-
-
+    public JavaBean() {
+    }
 
     public int getI() {
         return i;
@@ -35,5 +35,37 @@ public class JavaBean {
 
     public TmpClass getTmpClass() {
         return tmpClass;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        JavaBean javaBean = (JavaBean) o;
+
+        if (i != javaBean.i) return false;
+        if (isMan != javaBean.isMan) return false;
+        if (isWoman != null ? !isWoman.equals(javaBean.isWoman) : javaBean.isWoman != null) return false;
+        return tmpClass != null ? tmpClass.equals(javaBean.tmpClass) : javaBean.tmpClass == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = i;
+        result = 31 * result + (isMan ? 1 : 0);
+        result = 31 * result + (isWoman != null ? isWoman.hashCode() : 0);
+        result = 31 * result + (tmpClass != null ? tmpClass.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "JavaBean{" +
+                "i=" + i +
+                ", isMan=" + isMan +
+                ", isWoman=" + isWoman +
+                ", tmpClass=" + tmpClass +
+                '}';
     }
 }
