34aa586d9956ffb50095e9c147d9225a29367ff2
Andrei_Rohau <rohau.andrei@gmail.com>
2021-08-04 16:16:27 +0300

    lesson_02

---
+++ src/main/java/com/epam/Main.java
@@ -0,0 +1,29 @@
+package com.epam;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger log = Logger.getLogger(Main.class.getName());
+    public static void main(String[] args) {
+        log.info("asdasda");
+        int iiii = m1() + m2();
+        System.out.println(1 + 2 + "abc" + 1 + 3 + " sdf");
+
+        int[] ints = {1, 2};
+        System.out.println(ints.length);
+        ints[1] = 1;
+
+    }
+
+    public static int m1(){
+        log.info("1");
+        return 1;
+    }
+
+    public static int m2(){
+        log.info("2");
+        return 2;
+    }
+
+}
