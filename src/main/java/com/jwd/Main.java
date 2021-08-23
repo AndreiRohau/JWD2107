fb37832fc78702c7261d84d168966fd9aaddd2e6
Andrei_Rohau <rohau.andrei@gmail.com>
2021-08-23 11:31:36 +0300

    lesson 10

--- src/main/java/com/jwd/Main.java
+++
@@ -1,7 +0,0 @@
-package com.jwd;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/com/jwd/MainJar.java
@@ -0,0 +1,12 @@
+package com.jwd;
+
+public class MainJar {
+    public static void main(String[] args) {
+
+    }
+
+    public static String toUpperCase(String s) {
+        System.out.println("s=" + s);
+        return s.toUpperCase();
+    }
+}
